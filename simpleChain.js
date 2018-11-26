2a4d0eeb70c40d9cfcf98580e6d4a43831cca259
gowrieswaran <gowrieswar16@gmail.com>
2018-11-26 22:03:29 +0530

    Added functions to test and validate

--- simpleChain.js
+++ simpleChain.js
@@ -2,43 +2,44 @@
 |  Use this file to test your project.
 |  =========================================================*/
 
-const BlockChain = require('./BlockChain.js');
-const Block = require('./Block.js');
+const BlockChain = require("./BlockChain.js");
+const Block = require("./Block.js");
 
 let myBlockChain = new BlockChain.Blockchain();
 
-setTimeout(function () {
-	console.log("Waiting...")
+setTimeout(function() {
+  console.log("Waiting...");
 }, 10000);
 
 /******************************************
  ** Function for Create Tests Blocks   ****
  ******************************************/
 
-
-(function theLoop (i) {
-	setTimeout(function () {
-		let blockTest = new Block.Block("Test Block - " + (i + 1));
-		// Be careful this only will work if your method 'addBlock' in the Blockchain.js file return a Promise
-		myBlockChain.addBlock(blockTest).then((result) => {
-			console.log(result);
-			i++;
-			if (i < 10) theLoop(i);
-		});
-	}, 10000);
-  })(0);
-
+(function theLoop(i) {
+  setTimeout(function() {
+    let blockTest = new Block.Block("Test Block - " + (i + 1));
+    // Be careful this only will work if your method 'addBlock' in the Blockchain.js file return a Promise
+    myBlockChain.addBlock(blockTest).then(result => {
+      console.log(result);
+      i++;
+      if (i < 4) theLoop(i);
+    });
+  }, 10000);
+})(0);
 
 /***********************************************
  ** Function to get the Height of the Chain ****
  ***********************************************/
 
-/*
 // Be careful this only will work if `getBlockHeight` method in Blockchain.js file return a Promise
-myBlockChain.getBlockHeight().then((height) => {
-	console.log(height);
-}).catch((err) => { console.log(err);});
-*/
+myBlockChain
+  .getBlockHeight()
+  .then(height => {
+    console.log(height);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 /***********************************************
  ******** Function to Get a Block  *************
